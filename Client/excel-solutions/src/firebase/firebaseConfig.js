f56bf8aeb655d483c112f7a711c3e46024809b45
YuvalChen1 <pinewater700@gmail.com>
2024-10-28 18:03:28 +0200

    firebase-config

--- Client/excel-solutions/src/firebase/firebaseConfig.js
+++ Client/excel-solutions/src/firebase/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration object
 const firebaseConfig = {
-    apiKey: "AIzaSyDY75RjvDPnA4Xmi7e7ekORBlrkLrZHcoE",
-    authDomain: "excel-solutions-cc2be.firebaseapp.com",
-    projectId: "excel-solutions-cc2be",
-    storageBucket: "excel-solutions-cc2be.appspot.com",
-    messagingSenderId: "791627478399",
-    appId: "1:791627478399:web:f7519da4286effef64564a",
-    measurementId: "G-XLP65S2BFR"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
 // Initialize Firebase
